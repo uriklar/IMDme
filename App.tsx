22c4a71466ba97e2d677c1c1fcdcb4123f270788
Uri Klar <uriklar@gmail.com>
2019-02-07 22:14:59 +0200

    Set up routes and app skeleton

--- App.tsx
+++ App.tsx
@@ -8,46 +8,41 @@
  */
 
 import React, { Component } from "react";
-import { inject } from "mobx-react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import { Provider } from "mobx-react";
+import { ROUTES } from "./src/constants";
+import { createStackNavigator } from "react-navigation";
+import HomePage from "./src/components/home-page";
+import MoviePage from "./src/components/movie-page";
+import ActorPage from "./src/components/actor-page";
+import MovieStore from "./src/stores/movie-store";
 
 type Props = {};
 
-@inject("")
+const movieStore = new MovieStore();
+
+const RootStack = createStackNavigator(
+  {
+    [ROUTES.HOME]: {
+      screen: HomePage
+    },
+    [ROUTES.MOVIE]: {
+      screen: MoviePage
+    },
+    [ROUTES.ACTOR]: {
+      screen: ActorPage
+    }
+  },
+  {
+    initialRouteName: ROUTES.HOME
+  }
+);
+
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider movieStore={movieStore}>
+        <RootStack />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
